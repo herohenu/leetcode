ba0cf06c815c38dd7e8c9825f0a2973cab9c414b
lavin <lavin@iokde.com>
2021-11-16 21:01:20 +0800

    update

--- xiaochao/Lru.go
+++ xiaochao/Lru.go
@@ -1,5 +1,7 @@
 package main
 
+import "sync"
+
 /*
 @Time : 2021/11/16 19:46
 @desc labuladong  ch4 p24/66
@@ -9,16 +11,46 @@ package main
 // 因此使用 哈希链表
 
 type Node struct {
-	Key int
-	Val int
+	Key  int
+	Val  int
+	Pre  *Node
+	Next *Node
+	Freq int
 }
 
 type DoubleList struct {
-	Size int
+	Size int // 当前容量
+	lock *sync.RWMutex
+	Head *Node
+	Tail *Node
 }
 
 // 在链表头部添加节点 x，时间 O(1)
+// refer: https://segmentfault.com/a/1190000020042196
 func (list *DoubleList) AddFirst(node Node) {
+	if list == nil {
+		list = new(DoubleList)
+	}
+	if list.Size == 0 {
+		//false
+		return
+	}
+	list.lock.Lock()
+	defer list.lock.Unlock()
+
+	//判断头结点是否存在
+	if list.Head == nil {
+		list.Head = &node
+		list.Tail = &node
+	} else {
+		//头结点存在
+		list.Head.Next = &node
+		node.Next = list.Head
+
+		//设置新的头节点
+		list.Head = node
+
+	}
 
 }
 
@@ -34,7 +66,7 @@ func (list *DoubleList) removeLast() (node Node) {
 }
 
 type LruCache struct {
-	HashMap map[int]Node
+	HashMap map[int]Node // 删除list 中node 的时候 根据key 删除map中的val
 	List    *DoubleList
 	Cap     int
 }
@@ -47,22 +79,42 @@ func NewLruCache(limit int) *LruCache {
 	return cache
 }
 
+//todo
 func (cache LruCache) Put(key, val int) {
-	node := Node{Key: key, Val: val}
+	newNode := Node{Key: key, Val: val}
 
-	_, ok := cache.HashMap[node.Key]
+	oldNode, ok := cache.HashMap[key]
 	if ok {
 		//删除旧数据
+		cache.List.remove(oldNode)
 		// 新节点node 插入开头
+		cache.HashMap[key] = newNode
+		cache.List.AddFirst(newNode)
+
 	} else {
 		//cache 已满
 		if cache.List.Size >= cache.Cap {
 			//删除链表的最后一个数据腾位置
 			//删除 map 中映射到该数据的键
+			last := cache.List.removeLast()
+			delete(cache.HashMap, last.Key)
 
 		}
-		//将新节点 x 插入到开头；
+		// 将新节点 x 插入到开头；
 		// map 中新建 key 对新节点 x 的映射；
-
+		cache.List.AddFirst(newNode)
+		cache.HashMap[key] = newNode
 	}
+}
+
+func (cache LruCache) Get(key int) int {
+	nodeVal, ok := cache.HashMap[key]
+	if !ok {
+		return -1
+	}
+
+	//访问后 数据提前
+	cache.Put(key, nodeVal.Val)
+
+	return nodeVal.Val
 }
