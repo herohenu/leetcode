206e9d54287185cd0080167a8f9295e808c728a7
lavin <lavin@iokde.com>
2021-12-07 17:45:19 +0800

    update fix err

--- leet/15_threeNum.go
+++ leet/15_threeNum.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 )
 
+//
 func ThreeSum(nums []int) [][]int {
 	res := [][]int{}
 	m := make(map[string]bool)
@@ -12,19 +13,21 @@ func ThreeSum(nums []int) [][]int {
 	sort.Ints(nums)
 	for i := 0; i < len(nums); i++ {
 		target := 0 - nums[i]
-		fmt.Println("now  i is : ", i)
+		//fmt.Println("now  i is : ", i)
 		j := i + 1
 		k := len(nums) - 1
 		//fmt.Printf(" now idx   j %d , k %d \n", j, k)
 		for j < k {
 			if nums[j]+nums[k] == target {
-				fmt.Println("solution element is : ", []int{nums[i], nums[j], nums[k]})
+				//fmt.Println("solution element is : ", []int{nums[i], nums[j], nums[k]})
 				ijk := fmt.Sprintf("%d%d%d", nums[i], nums[j], nums[k])
-				fmt.Println("ijk is :", ijk)
+				//fmt.Println("ijk is :", ijk)
 				if _, ok := m[ijk]; !ok {
 					m[ijk] = true
 					res = append(res, []int{nums[i], nums[j], nums[k]})
 				}
+				j++
+				k--
 			} else if nums[j]+nums[k] > target {
 				k--
 			} else {
@@ -37,8 +40,9 @@ func ThreeSum(nums []int) [][]int {
 	return res
 }
 
-func ThreeSumMain() {
+func TestThreeSum() {
 	nums := []int{-2, 0, 1, 1, 2}
+	nums = []int{-4, -2, -1, -1, 0, 1, 1, 2}
 	// [[-2,0,2],[-2,1,1]]
 	sort.Ints(nums)
 	fmt.Println(nums)
